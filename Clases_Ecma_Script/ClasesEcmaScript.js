96bcf547f03a1cd6a6a605ba03fcb3a65e1d822e
Santiago Rivarola <srivarola@gmail.com>
2023-06-12 08:49:47 -0300

    manejo de archivos

--- Clases_Ecma_Script/ClasesEcmaScript.js
+++ Clases_Ecma_Script/ClasesEcmaScript.js
@@ -1,41 +1,54 @@
-const products = []
 
 class ProductManager {
 
-    constructor(title, description, price, thumbnail, code, stock) {
-        this.title = title;
-        this.description = description;
-        this.price = price;
-        this.thumbnail = thumbnail;
-        this.code = code;
-        this.stock = stock;
+    constructor() {
+        this.products = []
     };
 
-    addProduct(product) {
-        let id = 0
-        products.forEach(p => {
-            if(p.id > id) {
-                id = p.id
-            }
-        })
-        const codeRepetido = products.find(p => p.code == product.code)
-        if(product.title && product.description && product.price && product.thumbnail && product.code && product.stock != '' && !codeRepetido){
-            products.push({ ...product, id: id++ })
+    addProduct(title, description, price, thumbnail, code, stock) {
+        const codeRepetido = this.products.find(p => p.code == code)
+        const product = {
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock,
+            id: this.products.length + 1
+        }
+        if(title && description && price && thumbnail && code && stock != undefined && !codeRepetido){
+            this.products.push(product)
         } else {
             console.log('Complete all fields')
         }
     };
 
     getProducts() {
-        return products
-    }
+        return this.products
+    };
 
     getProductsById(id) {
-        const productById = products.filter(p => p.id == id)
+        
+        const productById = this.products.find(p => p.id == id)
         if (productById) {
             return productById
         }
-        return console.log("Not Found")
-    }
+        return console.error("Not Found")
+    };
 
 };
+
+const productos1 = new ProductManager()
+productos1.addProduct('Lapiz', 'Es un lapiz', 240, 'url imagen', 5431513, 100)
+productos1.addProduct('Goma', 'Es una goma', 200, 'url imagen', 1043250, 0)
+productos1.addProduct('Lapiz', 'Es un lapiz', 240, 'url imagen', 100)
+
+
+const productoId1 = productos1.getProductsById(1)
+
+
+productos1.getProductsById(4)
+
+console.log(productos1.getProducts())
+
+console.log(productoId1)
---
+++ Manejo_de_archivos/ProductManager.js
@@ -0,0 +1,181 @@
+const fs = require('fs');
+
+class ProductManager {
+
+    constructor(path) {
+        this.path = path;
+    };
+
+    addProduct = async(product) => {
+
+        try {
+
+            const products = await this.getProducts();
+            const codeRepetido = products.find(p => p.code == product.code);
+            
+            if(!product.title || !product.description || !product.price || !product.thumbnail || !product.code || product.stock == undefined){
+                return console.log('Complete all fields');
+            };
+            
+            if(codeRepetido) {
+                return console.log('The insert code already exist!');
+            };
+
+            const id = products.length + 1
+
+            products.push({
+                ...product,
+                id
+            });
+
+            await fs.promises.writeFile(this.path, JSON.stringify(products, null, '\t'));
+
+            return products
+
+        } catch (err) {
+            console.log(err);
+        };                   
+
+    };
+
+    getProducts = async() => {
+        try {
+            if(fs.existsSync(this.path)) {
+
+                const data = await fs.promises.readFile(this.path, 'utf-8');
+                console.log(data);
+                const parseData = JSON.parse(data);
+
+                return parseData;
+
+            } else {
+                return [];
+            };
+        } catch (err) {
+            console.log(err);
+        };
+    };
+
+    getProductsById = async(id) => {
+        try {
+            let resultado = await this.getProducts();
+            let product = resultado.find( p => p.id == id );
+
+            if(product) {
+                return console.log(product);
+            } else {
+                return console.error("Not Found");
+            };
+        } catch (err) {
+            console.log(err);
+        };
+    };
+
+    updateProduct = async(product) => {
+        try {
+
+            const products = await this.getProducts();
+            const productToUpdate = products.find( p => p.id == product.id );
+            
+            if(!productToUpdate) {
+                return console.log(`Can't find the product with id: ${product.id}`);
+            };
+            
+            const indexOfProduct = products.findIndex( p => p.id == product.id );
+
+            products[indexOfProduct] = {
+                ...productToUpdate,
+                ...product
+            };
+
+            await fs.promises.writeFile(this.path, JSON.stringify(products, null, '\t'));
+
+            return products[indexOfProduct];
+
+        } catch (err) {
+            console.log(err);
+        };
+
+    };
+
+    deleteProduct = async(id) => {
+        try {
+
+            const products = await this.getProducts();
+            const indice = products.findIndex( p => p.id === id );
+
+            if(indice < 0) {
+                return console.log(`Can't find the product with id: ${id}`);
+            };
+
+            products.splice(indice, 1);
+            await fs.promises.writeFile(this.path, JSON.stringify(products, null, '\t'));
+
+            return products;
+
+        } catch(err) {
+            console.log(err);
+        };
+    };
+
+};
+
+const manager = new ProductManager('./files/products.json');
+
+//producto completo
+const producto1 = {
+    title: 'Lapiz',
+    description: 'Es un lapiz',
+    price: 250,
+    thumbnail: 'url imagen',
+    code: 78651,
+    stock: 20
+}
+
+//producto completo
+const producto2 = {
+    title: 'Goma',
+    description: 'Es una goma',
+    price: 300,
+    thumbnail: 'url imagen',
+    code: 5431513,
+    stock: 20
+}
+
+// producto con stock 0, se agrega igual!!
+const producto3 = {
+    title: 'Cartuchera',
+    description: 'Es una cartuchera',
+    price: 1200,
+    thumbnail: 'url imagen',
+    code: 45684,
+    stock: 0
+}
+
+//producto con un campo faltante
+const producto4 = {
+    title: 'Algo',
+    description: 'Es algo',
+    price: 1200,
+    code: 45231,
+    stock: 0
+}
+
+const nuevosProductos = async() => {
+
+    await manager.addProduct(producto1);
+    await manager.addProduct(producto2);
+    await manager.addProduct(producto3);
+    await manager.addProduct(producto4);
+
+    // console.log(await manager.getProducts());
+
+    // console.log(await manager.getProductsById(1));
+
+    // console.log(await manager.updateProduct({id: 1, stock: 200}))
+
+    console.log(await manager.deleteProduct(1))
+
+}
+
+nuevosProductos()
